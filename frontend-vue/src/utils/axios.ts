0e9e000eb7f6386125807d247536a58b11b3cc14
DOBIN <devpark0810@gmail.com>
2025-01-30 22:49:48 +0900

    [update] EC2 연동 작업 01/30

--- frontend-vue/src/utils/axios.ts
+++ frontend-vue/src/utils/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: process.env.NODE_ENV === 'production' 
+        ? 'http://3.34.184.97'  // 프로덕션 URL
+        : 'http://localhost:3000',  // 개발 URL
     withCredentials: true
 });
 
