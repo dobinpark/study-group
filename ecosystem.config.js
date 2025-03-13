e8b7c5790b3ce2a9f09ad80cd8264ec6277e81ed
DOBIN <ff5026@naver.com>
2025-03-13 15:23:16 +0900

    [update] deploy.sh 리팩토링 03/13

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,14 +10,14 @@ module.exports = {
       NODE_ENV: 'production',
       PORT: 3000,
       CORS_ORIGIN: 'http://3.34.184.97',
-      DB_HOST: 'localhost', // ec2 내부에 mysql 서버가 있으므로 127.0.0.1또는 localhost로 설정
+      DB_HOST: '127.0.0.1', // ec2 내부에 mysql 서버가 있으므로 127.0.0.1또는 localhost로 설정
       DB_PORT: 3306,
       DB_USERNAME: 'root',
       DB_PASSWORD: 'Ddgh93081)',
       DB_DATABASE: 'study_group',
       SESSION_SECRET: '7f8e2b1d3e4f567a9b0c123d4e5f67890ab123cd4e5f6789',
       SESSION_MAX_AGE: 86400000,
-      REDIS_HOST: 'localhost',
+      REDIS_HOST: '127.0.0.1',
       REDIS_PORT: 6379,
     },
     // 로그 설정
